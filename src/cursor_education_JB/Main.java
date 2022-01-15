e105dfea45c7f2fe945a564a323b88756f13d0d4
Myaddib <Hotabich80@gmail.com>
2022-01-16 01:01:41 +0200

    Initial commit

---
+++ src/cursor_education_JB/Main.java
@@ -0,0 +1,18 @@
+package cursor_education_JB;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        RedBlackTree redBlackTree = new RedBlackTree();
+        redBlackTree.insert(15);
+        redBlackTree.insert(6);
+        redBlackTree.insert(5);
+        redBlackTree.insert(25);
+        redBlackTree.insert(27);
+        redBlackTree.insert(7);
+        redBlackTree.insert(19);
+
+    }
+}
